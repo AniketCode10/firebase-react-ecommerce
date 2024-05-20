b9131972469a43dc040fd76ea6516d76cd1d0940
aniket701661 <aniketchandivkar11@gmail.com>
2024-05-20 17:34:31 +0530

    files modified

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+        host: '0.0.0.0', // Bind to all available network interfaces
+        port: 5173, // Set your desired port number
+      },
+})
 
 // vite.config.js
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+// import { defineConfig } from 'vite';
+// import reactRefresh from '@vitejs/plugin-react-refresh';
 // export default defineConfig({
 
 
@@ -19,21 +23,22 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 //   },
 // });
 
-export default defineConfig({
-  plugins: [reactRefresh()],
-  server: {
-        host: '0.0.0.0', // Bind to all available network interfaces
-        port: 5173, // Set your desired port number
-      },
-  build: {
-    // Ensure production optimizations are enabled
-    base:"/",
-    minify: true,
-    terserOptions: {
-      compress: {
-        // Enable dead code elimination
-        dead_code: true,
-      },
-    },
-  },
-});
+// export default defineConfig({
+//   plugins: [reactRefresh()],
+//   server: {
+//         host: '0.0.0.0', // Bind to all available network interfaces
+//         port: 5173, // Set your desired port number
+//       },
+//   build: {
+//     // Ensure production optimizations are enabled
+    
+//     minify: 'terser',
+//     target: 'es2015',
+//     terserOptions: {
+//       compress: {
+//         // Enable dead code elimination
+//         dead_code: true,
+//       },
+//     },
+//   },
+// });
