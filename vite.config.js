08b64352ff1affe5e4cc2bf506fc9e658c20d697
aniket701661 <aniketchandivkar11@gmail.com>
2024-05-20 16:43:54 +0530

    config file modified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Bind to all available network interfaces
+    port: 5173, // Set your desired port number
+  },
+});
