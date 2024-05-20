bd7cc39409cc6a9e35d7b992423b87a4a2c0877c
aniket701661 <aniketchandivkar11@gmail.com>
2024-05-20 17:11:40 +0530

    files modified

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,32 @@
 
 // vite.config.js
 import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+// export default defineConfig({
+
+
+
+//   server: {
+//     host: '0.0.0.0', // Bind to all available network interfaces
+//     port: 5173, // Set your desired port number
+//   },
+// });
 
 export default defineConfig({
+  plugins: [reactRefresh()],
   server: {
-    host: '0.0.0.0', // Bind to all available network interfaces
-    port: 5173, // Set your desired port number
+        host: '0.0.0.0', // Bind to all available network interfaces
+        port: 5173, // Set your desired port number
+      },
+  build: {
+    // Ensure production optimizations are enabled
+    base:"/",
+    minify: true,
+    terserOptions: {
+      compress: {
+        // Enable dead code elimination
+        dead_code: true,
+      },
+    },
   },
 });
