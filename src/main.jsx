1613fae6a8d83705880de175a8ae9d2ba2cb7803
aniket701661 <aniketchandivkar11@gmail.com>
2024-07-14 01:30:32 +0530

    corrected naviagtes

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Provider store={store}>
-    <Routes>
+    
    
-    <Route path="/" element={ <App /> }>
-        </Route>
+    <App />
+      
    
-    </Routes>
+    
     </Provider>
     </BrowserRouter>
   </React.StrictMode>,
